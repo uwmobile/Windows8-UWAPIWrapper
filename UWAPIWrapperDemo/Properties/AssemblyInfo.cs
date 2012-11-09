9e0601bcf472faac123c7619638e53db8acb1737
Elton Gao <elton@uwmobile.ca>
2012-11-09 10:22:44 -0500

    - add more UI to it

--- UWAPIWrapperDemo/Properties/AssemblyInfo.cs
+++ UWAPIWrapperDemo/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UWAPIWrapperDemo")]
+[assembly: AssemblyTitle("Univ Waterloo API Wrapper Demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UWAPIWrapperDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyleft ©Elton Gao  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
